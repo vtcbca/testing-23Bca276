b9e14762869b4fe9e36473b07fc899c609689be1
23Bca276 <23Bca276@vtcbcsr.edu.in>
2024-09-10 10:47:54 +0530

    Add files via upload

---
+++ Assignment/GRADE.C
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+      char name[20]="griva";
+      char seatno[10];"24bca316";
+      char grade[10];
+      int cs,maths,ic,cppm,dpa,t,per;
+      clrscr();
+      printf("cs=");
+      scanf("%d",&cs);
+      printf("maths=");
+      scanf("%d",&maths);
+      printf("cppm=");
+      scanf("%d",&cppm);
+      print("ic=");
+      scanf("%d",&ic);
+      printf("dpa=");
+      scanf("%d",dpa);
+      {
+       g="distiniction";
+      }
+   else if(per>=80 && per<=89)
+      {
+	g="first class";
+      }
+  else if (per)= 70 && per<=79)
+      {
+	g= "second class";
+      }
+   else if (per>=60 && per<=69)
+      {
+	g= "third class";
+      }
+   else if (per> = 50 && per<=59)
+      {
+	g="pass class";
+      }
+   else (below 50)
+      {
+	g= "fail";
+      }
+      printf("\n****marksheet****);
+      printf("\n student seatno:%s",seatno);
+      printf("\nstudent name:%s",name);
+      printf("\n===markdetail===");
+      printf("\n cs\t maths\t cppm\t ic\t dpa\t total\t per");
+      printf("\n %d\t %d\t %d\t %d\t %d\t %d\t %d\t",cs,maths,cppm,ic,dpa,t,
+      per,grade);
+      if(per>=90)
+      {
+	printf("distinction")
+      }
